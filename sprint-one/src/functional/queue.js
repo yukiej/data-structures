6d20a46bb6074475a6a27f48f1e1e2c5ba0173e8
Yuki Jung <yukiej@gmail.com>
2018-09-19 11:46:29 -0700

    Finish methods that pass all tests for functional pattern queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0; 
+  var numRemoved = 0; 
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    count++;
+    storage[count] = value;
   };
 
   someInstance.dequeue = function() {
+    if (count > 0){
+      count--;
+      numRemoved++;
+      return storage[1 + numRemoved];
+
+    }
   };
 
   someInstance.size = function() {
+    return count; 
   };
 
   return someInstance;
