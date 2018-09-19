b7d0cb36dd30b2ac1dcefc3b662e5a1d1653643d
Yuki Jung <yukiej@gmail.com>
2018-09-19 15:27:54 -0700

    Finish es6 queue that passes all tests

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,25 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+  	this.count = 0;
+  	this.removed = 0;
+  	this.storage = {};
+  }
+
+  size() {
+  	return this.count - this.removed;
+  }
+
+  enqueue(value) {
+  	this.count++;
+  	this.storage[this.count] = value;
+  }
+
+  dequeue() {
+  	if ((this.count - this.removed) > 0) {
+  		var result = this.storage[1 + this.removed];
+  		this.removed++;
+  		return result;
+  	}
   }
 
 }
