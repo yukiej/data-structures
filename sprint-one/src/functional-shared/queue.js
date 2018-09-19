24e5d8efed4f2ca37a0cd593af3c617892c69156
Yuki Jung <yukiej@gmail.com>
2018-09-19 13:10:00 -0700

    Finish implementing functional-shared queue that passes all tests

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,29 @@
+
+
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+	var obj = {};
+	obj.storage = {};
+	obj.count = 0;
+	obj.removed = 0; 
+  	$.extend(obj, queueMethods);
+  	return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+	size : function() {
+		return this.count - this.removed;
+	},
+	enqueue : function(value) {
+	 	this.count++
+	 	this.storage[this.count] = value;
+	},
+	dequeue : function(){
+		if (this.count - this.removed > 0){
+			var result = this.storage[1 + this.removed];
+			this.removed++;	
+			return result;
+		}
+	}
+};
 
 
