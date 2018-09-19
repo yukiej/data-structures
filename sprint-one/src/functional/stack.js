0b64c51b6d64d229b99928d62ca9be096f9ad20d
Yuki Jung <yukiej@gmail.com>
2018-09-19 11:36:14 -0700

    Finish methods that pass all tests

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,25 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0; 
 
   // Implement the methods below
   someInstance.push = function(value) {
+    count++;
+    storage[count] = value;
   };
 
   someInstance.pop = function() {
+    if (count > 0) {
+      var result = storage[count];
+      delete storage[count];
+      count--;
+      return result; 
+    }
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
