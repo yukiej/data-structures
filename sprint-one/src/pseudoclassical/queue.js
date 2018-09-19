05d63a378de304d9301f9ebb66656d94688cc65d
Yuki Jung <yukiej@gmail.com>
2018-09-19 15:10:18 -0700

    Finish pseudoclassical queue that passes all tests

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,25 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  this.storage = {};
+  this.removed = 0;
 };
+
+Queue.prototype.size = function() {
+	return this.count - this.removed;
+}
+
+Queue.prototype.enqueue = function(value) {
+	this.count++;
+	this.storage[this.count] = value
+}
+
+Queue.prototype.dequeue = function() {
+	if (this.count - this.removed > 0) {
+		var result = this.storage[1 + this.removed]
+		this.removed++;
+		return result;
+	}
+	
+}
 
 
