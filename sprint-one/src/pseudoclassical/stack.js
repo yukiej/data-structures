f6b3a53a75ecefd4a8b39c05a4d319cde2c30776
Yuki Jung <yukiej@gmail.com>
2018-09-19 14:58:30 -0700

    Finish pseudoclassical queue that passes all tests

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,22 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+	this.count = 0; 
+	this.storage = {};
 };
 
 
+Stack.prototype.size = function() {
+	return this.count;
+}
+
+Stack.prototype.push = function(value) {
+	this.count++;
+	this.storage[this.count] = value;
+}
+
+Stack.prototype.pop = function() {
+	if (this.count > 0) {
+		var result = this.storage[this.count];
+		this.count--;	
+		return result;
+	}
+}
