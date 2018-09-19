8729426899c5b08815adac874e3af135f046616a
Yuki Jung <yukiej@gmail.com>
2018-09-19 14:39:32 -0700

    Finish stack for prototypal pattern

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,25 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+  obj.count = 0; 
+  obj.storage = {};
+  return obj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+	size : function() {
+		return this.count;
+	},
+	push : function(value) {
+		this.count++;
+		this.storage[this.count] = value;
+	},
+	pop : function() {
+		if (this.count > 0) {
+			var result = this.storage[this.count];
+			this.count--;
+			return result;
+		}
+	}
+};
 
 
