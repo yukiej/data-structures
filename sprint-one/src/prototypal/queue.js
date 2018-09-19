90772deaffa57d0befe94ae1634fcda6f7221547
Yuki Jung <yukiej@gmail.com>
2018-09-19 14:52:03 -0700

    Finish prototypal pattern stack that passes all tests

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,27 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+	var obj = Object.create(queueMethods);
+	obj.count = 0; 
+	obj.removed = 0; 
+	obj.storage = {};
+	return obj;
+  
 };
 
-var queueMethods = {};
+var queueMethods = {
+	size: function() {
+		return this.count - this.removed;
+					},
+	enqueue: function(value) {
+		this.count++;
+		this.storage[this.count] = value;
+					},
+	dequeue: function() {
+		if (this.count - this.removed > 0) {
+			result = this.storage[1 + this.removed];
+			this.removed++;
+			return result; 
+		}
+	}
+};
 
 
