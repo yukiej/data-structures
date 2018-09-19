b8ee31f72866383f2caa5246956df3287acabaf7
Yuki Jung <yukiej@gmail.com>
2018-09-19 13:18:00 -0700

    Finish functional-shared pattern for stack that passes all tests

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+	var obj = {}
+	obj.storage = {}
+	obj.count = 0; 
+	$.extend(obj, stackMethods);
+	return obj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+	size: function(){
+		return this.count;
+		},
+	push: function(value){
+		this.count++
+		this.storage[this.count] = value;
+
+	},
+	pop: function(){
+		if (this.count > 0) {
+			var result = this.storage[this.count];
+			this.count--	
+			return result; 
+		}
+	}
+};
 
 
