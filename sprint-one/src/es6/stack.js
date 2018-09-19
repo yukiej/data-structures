fdd4de78b4ecfcc913e7a8703854d20b7e4ef385
Yuki Jung <yukiej@gmail.com>
2018-09-19 15:20:52 -0700

    Finish es6 stack that passes all tests

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,23 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+  	this.count = 0;
+  	this.storage = {};
   }
 
+  size() {
+  	return this.count;
+  }
+
+  push(value) {
+  	this.count++;
+  	this.storage[this.count] = value;
+  }
+
+  pop() {
+  	if (this.count > 0) {
+  		var result = this.storage[this.count];
+  		this.count--;
+  		return result;
+  	}
+  }
 }
