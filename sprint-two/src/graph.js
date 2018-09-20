91ff24f5e9122017990edae21dd2df32a2028633
yukiej <yukiej@gmail.com>
2018-09-20 14:04:33 -0700

    Finish addNode and contains for Graph that passes first two tests

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,15 +1,42 @@
 
 
 // Instantiate a new graph
-var Graph = function() {
+var Graph = function(val) {
+  this.connections = [];
+  this.value = val;
 };
+
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  var newGraph = new Graph(node);
+  this.connections.push(newGraph);
+  newGraph.connections.push(this);
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  var visitedNodes = [];
+  
+  var find = function() {
+
+    if (node === this.value) {
+      return true;
+    } else {
+      visitedNodes.push(this);
+      var edges = this.connections;
+      console.log(this, edges); 
+      for (var i = 0; i < edges.length; i++) {
+        if (! (visitedNodes.includes(edges[i]))) {
+          if (find.call(edges[i])) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  };
+  return find.call(this);
 };
 
 // Removes a node from the graph.
